ecba845a43b333a50501b68cd41c59abb2c89961
Darya Yusipets <dyusipets@gmail.com>
2022-02-13 22:40:00 +0300

    added Head data

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,7 +1,16 @@
 import ContactForm from "./../../components/contact/contact-form";
+import Head from "next/head";
 
 const ContactPage = () => {
-  return <ContactForm />;
+  return (
+    <>
+      <Head>
+        <title>Contact me</title>
+        <meta name="description" content="Send me your messsages" />
+      </Head>
+      <ContactForm />
+    </>
+  );
 };
 
 export default ContactPage;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeturedPosts } from "../lib/posts-util";
 import Hero from "./../components/home-page/hero";
 
 const HomePage = (props) => {
   <>
+    <Head>
+      <title>Dasha` Blog</title>
+      <meta name="description" content="This is my blog" />
+    </Head>
     <Hero />
     <FeaturedPosts posts={props.posts} />
   </>;
