63c92cbe19b2ed3d8184592845271374dd8dfeb1
Darya Yusipets <dyusipets@gmail.com>
2022-02-13 16:59:13 +0300

    added markdown files

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,21 @@
 import FeaturedPosts from "../components/home-page/featured-posts";
+import { getFeturedPosts } from "../lib/posts-util";
 import Hero from "./../components/home-page/hero";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJs is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-12-03",
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJs is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-12-03",
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJs is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-12-03",
-  },
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJs is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-12-03",
-  },
-];
-
-const HomePage = () => {
+const HomePage = (props) => {
   <>
     <Hero />
-    <FeaturedPosts posts={DUMMY_POSTS} />
+    <FeaturedPosts posts={props.posts} />
   </>;
 };
+
+export function getStaticProps() {
+  const featuredPosts = getFeturedPosts();
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
+}
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,17 @@
-const AllPostsPage = () => {};
+import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "./../../lib/posts-util";
+
+const AllPostsPage = (props) => {
+  return <AllPosts posts={props.posts} />;
+};
+
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
+}
 
 export default AllPostsPage;
